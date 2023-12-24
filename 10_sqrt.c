2a77d318d0348611709a230d5bf62fb5f61032cd
Harsh-Pratap-Singh <harshpratapsingh.main@gmail.com>
2023-12-24 21:31:05 +0530

    10 question

---
+++ 10_sqrt.c
@@ -0,0 +1,30 @@
+int mySqrt(int x){
+    return sqrt(x);
+}
+
+
+
+
+
+
+
+// int mySqrt(int x) {
+//     long long i=1;
+// if(x<=0){
+//     return 0;
+// }
+// if(x==1){
+//     return 1;
+// }
+
+//     while(i<=x/2){
+//         if(i*i <= x && x < (i+1)*(i+1) ){
+//             return (i);         }
+//         else{
+//             i++;
+//         }
+
+//     }
+//     return -1;
+    
+// }
