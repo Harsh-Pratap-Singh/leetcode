92c17eb45677dc7990f77c1d56ac7764ee4d5fa0
Harsh-Pratap-Singh <harshpratapsingh.main@gmail.com>
2024-01-26 14:53:49 +0530

    46th question

---
+++ 46_find_3_consecut_int_sum_to_given_num.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<long long> sumOfThree(long long num) {
+        vector<long long>ans;
+        if(num%3 != 0){
+            return {};
+        }
+        else{
+            long long a = num/3;
+            ans.push_back(a - 1);
+            ans.push_back(a);
+            ans.push_back(a + 1);
+        }
+        return ans;
+    }
+};
